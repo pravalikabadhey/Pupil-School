58908c143979435e43c2dddc5fe4b212b0889e0a
pravalikabadhey <103028045+pravalikabadhey@users.noreply.github.com>
2022-09-16 11:58:17 +0530

    Add files via upload

---
+++ done.js
@@ -0,0 +1,28 @@
+* {
+    margin: 0;
+    padding: 0;
+    box - sizing: border - box;
+    font - family: cambria;
+}
+body {
+    background - color: antiquewhite;
+    font - size: 18 px;
+}
+.box {
+    display: flex;
+    align - items: center;
+    justify - content: center;
+    margin - top: 3 rem;
+}
+.form {
+    display: flex;
+    align - items: center;
+    flex - direction: column;
+    background - color: white;
+    width: 600 px;
+    height: 500 px;
+    box - shadow: 2 px 4 px 10 px gray;
+    border: 1 px solid black;
+    border - radius: 20 px;
+    padding: 20 px;
+}
